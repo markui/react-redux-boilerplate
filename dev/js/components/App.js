0bede3328e6dd0c811302d641d0d13af2be7eb6c
Mark Kim <viking617617@gmail.com>
2018-01-09 17:58:48 +0900

    added Containers

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import UserList from '../containers/user-list';
 require('../../scss/style.scss');
 
 const App = () => (
   <div>
     <h1>Username list:</h1>
+    <UserList />
     <hr/>
     <h2>User Details:</h2>
   </div>
 );
-
-export default App;
