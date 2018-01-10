fdde7452b898b3d1780e8fa7b3cbaa5b7033f414
Mark Kim <viking617617@gmail.com>
2018-01-10 16:52:52 +0900

    added ActiveUser Reducer to listen to Actions

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
-import UserReducer from './reducer-users.js';
+import UserReducer from './reducer-users';
+import ActiveUserReducer from './reducer-active-user';
 
 // we need a main javascript object that is going to be throwed inside our Store
 const allReducers = combineReducers({
-  users: UserReducer
+  users: UserReducer,
+  activeUser: ActiveUserReducer
 });
 
 export default allReducers;
